8ef876365174267194c5f0f3de44d1e3355712e9
Pierre Rochard <pierre@rochard.org>
2016-07-30 08:49:43 -0400

    flask admin experiment was successful

--- realtime/webserver/views.py
+++ realtime/webserver/views.py
@@ -1,4 +1,3 @@
-from flask import render_template, redirect, Blueprint, url_for
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 
@@ -7,19 +6,8 @@ from realtime.database.adapter import db
 
 admin = Admin(name='realtime', template_mode='bootstrap3', url='/')
 
+
 class RealtimeModelView(ModelView):
     list_template = 'realtime.html'
 
 admin.add_view(RealtimeModelView(Updates, db.session))
-
-# @blueprint.route('/')
-# def index():
-#     updates = Updates.query.all()
-#     return render_template('index.html', updates=updates)
-#
-#
-# @blueprint.route('/delete')
-# def delete():
-#     Updates.query.delete()
-#     db.session.commit()
-#     return redirect(url_for('.index'))
