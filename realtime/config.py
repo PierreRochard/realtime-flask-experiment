2c8d547287c1c7ace6a16e6c07da079a52ebbe34
Pierre Rochard <pierre@rochard.org>
2016-09-09 18:42:28 -0400

    progress

--- realtime/config.py
+++ realtime/config.py
@@ -1,5 +1,6 @@
-
+import os
 
 SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://localhost/realtime'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 DEBUG = True
+SECRET_KEY = os.urandom(24)
--- realtime/webserver/views.py
+++ realtime/webserver/views.py
@@ -1,3 +1,7 @@
+import uuid
+
+from flask import request, session
+
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 
@@ -9,5 +13,16 @@ admin = Admin(name='realtime', template_mode='bootstrap3', url='/')
 
 class RealtimeModelView(ModelView):
     list_template = 'realtime.html'
+
+    def pre_render_function(self):
+        session['session_id'] = uuid.uuid4()
+        print(request)
+        from pprint import pformat
+        print(pformat(dir(request)))
+        print(session)
+        from pprint import pformat
+        print(pformat(dir(session)))
+        print(session.keys())
+
 
 admin.add_view(RealtimeModelView(Updates, db.session))
