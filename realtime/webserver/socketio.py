2274706b69cd6751644231e65e285a1db3fedb48
Pierre Rochard <pierre@rochard.org>
2016-09-10 13:02:29 -0400

    debugging

--- realtime/webserver/socketio.py
+++ realtime/webserver/socketio.py
@@ -1,6 +1,10 @@
+from flask import session, request
+
 from flask_socketio import SocketIO
 
 from realtime.webserver.webapp import app
+from realtime.database.adapter import db
+from realtime.database.models import SocketIoSessions
 
 socketio = SocketIO(app)
 
@@ -17,9 +21,26 @@ def on_disconnect():
 
 @socketio.on('connect', namespace='/browser')
 def on_connect_browser():
+    session['socket_io_id'] = request.sid
+    new_socket_io_session = dict(session)
+    new_socket_io_session = SocketIoSessions(**new_socket_io_session)
+    db.session.add(new_socket_io_session)
+    db.session.commit()
     print('SocketIO connect /browser')
+
+
+@socketio.on('ids', namespace='/browser')
+def on_ids(message):
+    print(message)
 
 
 @socketio.on('disconnect', namespace='/browser')
 def on_disconnect_browser():
+    disconnected_session = (
+        db.session.query(SocketIoSessions)
+            .filter(SocketIoSessions.socket_io_id == session['socket_io_id'])
+            .one()
+    )
+    db.session.delete(disconnected_session)
+    db.session.commit()
     print('SocketIO disconnect /browser')
